c543477a7e9581f5ac567319d50378fd3808f7fd
Petr Shypila <iKrumping@gmail.com>
2018-02-10 16:43:28 +0100

    Updated findByUsername

--- app/actions/userActions.js
+++ app/actions/userActions.js
@@ -3,19 +3,16 @@ import {User} from '../models';
 import logger from '../logging';
 
 export function findByUsername(req, res) {
-  const {username} = req.body;
-  const {password} = req.body;
+  const {username, password} = req.body;
 
   logger.info(`Received request to login user ${username}`);
 
   User.findOne({username: username.toLowerCase()}).then(user => {
-    if (!user) { // No user with provided username
-      res.status(404).end();
-      logger.info(`User ${username} does not exist`);
-    } else {
+    if (user) { // No user with provided username
       bcrypt.compare(password, user.password, (err, isValid) => {
         if (err) {
-          res.status(500).json(err);
+          logger.error(`Error during password validation of user ${user.username}`);
+          res.status(500).json({error: err.message});
         } else if (!isValid) {
           logger.info(`User ${username} provided incorrect password`);
           res.status(401).end();
@@ -24,10 +21,13 @@ export function findByUsername(req, res) {
           res.status(200).json(user);
         }
       });
+    } else {
+      res.status(404).end();
+      logger.info(`User ${username} does not exist`);
     }
   }).catch(err => {
     logger.info(`Error appeared while processing user ${username}`);
-    res.status(500).json(err);
+    res.status(500).json({error: err.message});
   });
 }
 
@@ -36,15 +36,16 @@ function createHash(password){
 }
 
 export function saveUser(req, res) {
-  const {username} = req.body;
+  const {username, password} = req.body;
+
   User.findOne({username: username.toLowerCase()}).then(user => {
     if (!user) { // No user with provided username
-      req.body.password = createHash(req.body.password);
+      req.body.password = createHash(password);
       let newUser = new User(req.body);
 
       newUser.save(err => {
         if (err) {
-          res.status(500).send('Error saving user to database');
+          res.status(500).json({message: 'Error saving user to database'});
         } else {
           res.status(201).json(user);
         }
